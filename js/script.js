44aa308ff718e1dbaaaf8ed318d9e1f8cac3edfe
Akbarss <kbrgulyamov@gmail.com>
2024-01-27 11:30:27 +0500

    feat: add quiz data obj

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const quizData = [
+  {
+    question: "Which language runs in a web browser?",
+    a: "Java",
+    b: "C",
+    c: "Python",
+    d: "JavaScript",
+    correct: "",
+  },
+  {
+    question: "What does CSS stand for?",
+    a: "Central Style Sheets",
+    b: "Cascading Style Sheets",
+    c: "Cascading Simple Sheets",
+    d: "Cars SUVs Sailboats",
+    correct: "",
+  },
+  {
+    question: "What does HTML stand for?",
+    a: "Hypertext Markup Language",
+    b: "Hypertext Markdown Language",
+    c: "Hypertext Machine Language",
+    d: "Cars SUVs Sailboats",
+    correct: "",
+  },
+  {
+    question: "What year was JavaScript launched?",
+    a: "1996",
+    b: "1995",
+    c: "1994",
+    d: "none of the above",
+    correct: "",
+  },
+];
